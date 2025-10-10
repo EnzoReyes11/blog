c2b800edb8ac799cda0d24fc10a3d23ee35eaa5d
Enzo Reyes <reyes.enzo1985@gmail.com>
2025-10-10 15:57:06 +0000

    [ADD] Fonts are defined in Tailwind now.
    This makes using fonts much easier. Fixed some of the problems I was having.

--- portfolio/tailwind.config.mjs
+++ portfolio/tailwind.config.mjs
@@ -1,8 +1,17 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import typography from '@tailwindcss/typography';
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				plex: ['IBMPlexSans', ...defaultTheme.fontFamily.sans],
+				vcr: ['VCR', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [typography],
 };
